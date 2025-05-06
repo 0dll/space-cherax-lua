3fa635e7bcee2610ccedb468215047628c948785
Caldwell <103705076+0dll@users.noreply.github.com>
2025-05-05 21:07:29 -0400

    Update !self_options.lua

--- space_lib/!self_options.lua
+++ space_lib/!self_options.lua
@@ -1,7 +1,7 @@
 
 function _self_options_main_()
 
-    _hot_key_tbl.ped_wetness, _settings.ped_wetness_value = add_toggle_editor_option("Ped Wetness", "Why Exactly Are You Using This -_-", submenus.self_main_menu, _hot_key_tbl.ped_wetness, true, _settings.ped_wetness, _settings.ped_wetness_value, -1.10, 0.75, 0.05, function() to_enable.option_ped_wetness(true) end)
+    _hot_key_tbl.ped_wetness, _settings.ped_wetness_value = add_toggle_editor_option("Ped Wetness", "", submenus.self_main_menu, _hot_key_tbl.ped_wetness, true, _settings.ped_wetness, _settings.ped_wetness_value, -1.10, 0.75, 0.05, function() to_enable.option_ped_wetness(true) end)
 
     _hot_key_tbl.disable_landing_anim = add_toggle_option("Instant Land", " ", submenus.self_main_menu, _hot_key_tbl.disable_landing_anim, true, _settings.disable_landing_anim, function() to_enable.disable_landing_anim_func(true) end)
 
